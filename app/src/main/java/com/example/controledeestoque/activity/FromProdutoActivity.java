92ae6c59dc687e2721a5db7d73d3e69ac7e1e3ea
Jade Santos <jade.santos@sp.agence.com.br>
2023-02-05 19:38:15 -0400

    add abrir galeria e verificar permissão de acesso

--- app/src/main/java/com/example/controledeestoque/activity/FromProdutoActivity.java
+++ app/src/main/java/com/example/controledeestoque/activity/FromProdutoActivity.java
@@ -2,22 +2,34 @@ package com.example.controledeestoque.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.controledeestoque.R;
 import com.example.controledeestoque.model.Produto;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class FromProdutoActivity extends AppCompatActivity {
 
+    private static  final int REQUEST_GALERIA = 100;
+    private ImageView imagem_produto;
     private EditText edit_produto;
     private EditText edit_quantidade;
     private EditText edit_valor;
 
     private Button salvarProduto;
     private Produto produto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +47,40 @@ public class FromProdutoActivity extends AppCompatActivity {
         salvarProduto.setOnClickListener( v-> salvarProduto());
     }
 
+    public void abrirGaleria(View view){
+        verificaPermissaoGaleria();
+    }
+
+    private void verificaPermissaoGaleria(){
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                acessarGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(FromProdutoActivity.this, "Permissão Negada.", Toast.LENGTH_SHORT).show();
+            }
+        };
+        showDialogPermissao(permissionListener, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE});
+    }
+
+    private void acessarGaleria(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_GALERIA);
+    }
+
+    private void showDialogPermissao(PermissionListener listener, String[] permissoes){
+        TedPermission.create()
+                .setPermissionListener(listener)
+                .setDeniedTitle("Permissões")
+                .setDeniedMessage("Permissão negada para acessar a galeria do dispositivo, deseja permitir?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(permissoes)
+                .check();
+    }
 
     private void editProduto(){
         edit_produto.setText(produto.getNome());
@@ -94,11 +140,11 @@ public class FromProdutoActivity extends AppCompatActivity {
 
     }
 
-
-        private void inicializarComponentes () {
+    private void inicializarComponentes () {
             edit_produto = findViewById(R.id.editCadProduto);
             edit_quantidade = findViewById(R.id.editCadEstoque);
             edit_valor = findViewById(R.id.editCadValor);
             salvarProduto = findViewById(R.id.btnSalvar);
+            imagem_produto = findViewById(R.id.imagem_produto);
         }
     }
